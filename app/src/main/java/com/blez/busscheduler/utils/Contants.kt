aa69e73ad35cdf8c25db5c0070b8a3ed0f828162
Blez_Ten <83898491+blezDev@users.noreply.github.com>
2022-10-23 02:14:07 +0530

    ADDED Token Manager

---
+++ app/src/main/java/com/blez/busscheduler/utils/Contants.kt
@@ -0,0 +1,9 @@
+package com.blez.busscheduler.utils
+
+object Contants {
+    const val BASE_URL = ""
+    const val RE_TAG = "RE_TAG"
+    const val PREFS_TOKEN_FILE = "PREFS_TOKEN_FILE"
+    const val USER_TOKEN = "USER_TOKEN"
+    const val USER_EMAIL = "USER_EMAIL"
+}
---
+++ app/src/main/java/com/blez/busscheduler/utils/TokenManager.kt
@@ -0,0 +1,42 @@
+package com.blez.busscheduler.utils
+
+import android.content.Context
+import android.content.SharedPreferences
+import com.blez.busscheduler.utils.Contants.PREFS_TOKEN_FILE
+import com.blez.busscheduler.utils.Contants.USER_EMAIL
+import com.blez.busscheduler.utils.Contants.USER_TOKEN
+
+class TokenManager(context: Context) {
+    private var prefs : SharedPreferences = context.getSharedPreferences(PREFS_TOKEN_FILE, Context.MODE_PRIVATE)
+
+    fun saveToken(token : String)
+    {
+        val editor = prefs.edit()
+        editor.putString(USER_TOKEN,token)
+        editor.apply()
+    }
+    fun saveEmail(email : String)
+    {
+        val editor = prefs.edit()
+        editor.putString(USER_EMAIL,email)
+        editor.apply()
+    }
+    fun getToken() : String?
+    {
+        return prefs.getString(USER_TOKEN,null)
+    }
+    fun getEmail() : String?{
+        return prefs.getString(USER_EMAIL,null)
+    }
+    fun deteleCredit(){
+        if(prefs.getString(USER_TOKEN,null) != null && prefs.getString(USER_EMAIL,null) != null)
+        {
+            val editor = prefs.edit()
+            editor.clear()
+            editor.apply()
+        }
+
+    }
+
+
+}
